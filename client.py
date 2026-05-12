d348d34ce2e1b5baad65e74288cbdfbb231f5aa0
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:42:05 -0300

    feat: crear la funcion conectar que se encarga de crear e intentar conectar el socket del cliente al servidor 10 veces en intervalos de 2 segundos

--- client.py
+++ client.py
@@ -7,9 +7,32 @@ import time      # Libreria que importa herramientos de tiempo para interactuar 
 
 
 #====================================================
-# BLOQUE 2: CONFIGURACION DEL SERVIDOR
+# BLOQUE 2: CONFIGURACION DEL SOCKET
 #====================================================
 HOST = 'localhost' # te conecta a la misma computadora en donde corre el servidor
 PUERTO = 8000      # este es el canal de comunicacion 
 MAX_INTENTOS  = 10
 TIEMPO_ESPERA = 2
+
+
+#====================================================
+# BLOQUE 3: CREACION DEL SOCKET CLIENTE Y CONEXION
+#====================================================
+
+def conectar(): 
+
+    intento = 1
+    while intento <= MAX_INTENTOS: # Bucle de conexion con el servidor
+        
+        try:
+            # --- Creacion del socket Y Asociacion Puerto/IP ---
+            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea un objeto socket de la clase socket con los protocolos IPv4 (AF_INET) y TCP (SOCK_STREAM)
+            cliente.connect((HOST, PUERTO)) # Conecta el sokect cliente al sevidor con la IP y puerto indicados
+
+            print(f"[✅ ] Conectado al servidor en el intento {intento}.")
+            return cliente # Si se conecta, sale del bucle)
+            
+        except ConnectionRefusedError:
+            print(f"[⚠️ ] Intento {intento} fallido. Reintentando en 2 segundos...")
+            intento += 1
+            time.sleep(2)
