a7055f7f64aa23de12e43c0997bbda776ed7de4c
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:39:59 -0300

    feat: crear la configuracion para el socket

--- client.py
+++ client.py
@@ -4,3 +4,12 @@
 import socket    # Libreria que nos proporciona una interfaz de bajo ivel para la creacion de procesos de conexion
 import threading # permite ejecutar varias cosas al mismo tiempo en este caso leer y escribir sin que una bloquee a la otra
 import time      # Libreria que importa herramientos de tiempo para interactuar con el programa
+
+
+#====================================================
+# BLOQUE 2: CONFIGURACION DEL SERVIDOR
+#====================================================
+HOST = 'localhost' # te conecta a la misma computadora en donde corre el servidor
+PUERTO = 8000      # este es el canal de comunicacion 
+MAX_INTENTOS  = 10
+TIEMPO_ESPERA = 2
