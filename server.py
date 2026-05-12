c8c164ebd42f767a52375e1a8ee252bfdd211d47
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:24:46 -0300

    feat: agregar la configuracion inicial definiendo el HOST y PUERTO del servvidor

--- server.py
+++ server.py
@@ -3,3 +3,10 @@
 #=====================================================
 import socket # Libreria que proporciona las funciones y clases necesarias para crear y manipular sockets
 import select # Función que permite vigilar múltiples sockets simultaneamente para realizar alguna operación (leer, escribir o detectar errores), sin necesidad de usar hilos.
+
+
+#=====================================================
+# BLOQUE 2: CONFIGURACION INICIAL
+#=====================================================
+HOST = 'localhost' # 127.0.0.1
+PUERTO = 8000 # para desarrollo local aunque es conocido como puerto utilizado por malware, podria utilizarse el piuerto 8000
