1f9fca702b0023ab64bce399f7ba97380d0ed881
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:27:21 -0300

    feat: agregar la creacion y configuracion del sokect

--- server.py
+++ server.py
@@ -10,3 +10,23 @@ import select # Función que permite vigilar múltiples sockets simultaneamente 
 #=====================================================
 HOST = 'localhost' # 127.0.0.1
 PUERTO = 8000 # para desarrollo local aunque es conocido como puerto utilizado por malware, podria utilizarse el piuerto 8000
+
+
+#=======================================================
+# BLOQUE 3: CREACION Y CONFIGURACION DEL SOCKET SERVIDOR
+#=======================================================
+
+# Creamos un sokect con conexiones IPV4 como puertos de 192.168.0.1  y del tipo TCP 
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# configurar las opcion internsa del socket. sol socket nivel donde se aplica la opcion a nivel de socket no del protoclo, en el servidor para reutilizar el host y puerto 
+servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+servidor.bind((HOST, PUERTO)) # Asocia el socket a una direccion IP y Puerto (con .bind)
+
+
+servidor.listen()
+# basicamente le dice al sistema, estoy listo para recibir conexiones, empieza a escuchar modo servidor pasivo, pero aun no acepta ninguna conexion
+
+# Imprime en el chat del servidor los datos del puerto y anfritrion
+print(f"[🎧] Servidor escuchando en {HOST}:{PUERTO}")
