add1481f130c19cef15b2074246729360702350c
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:23:39 -0300

    feat: agregar librerias de select y sokects necesarias para la construccion del servidor

---
+++ server.py
@@ -0,0 +1,5 @@
+#=====================================================
+# BLOQUE 1: LIBRERIAS
+#=====================================================
+import socket # Libreria que proporciona las funciones y clases necesarias para crear y manipular sockets
+import select # Función que permite vigilar múltiples sockets simultaneamente para realizar alguna operación (leer, escribir o detectar errores), sin necesidad de usar hilos.
