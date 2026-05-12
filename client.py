0d23f898ccd967df8a14c79b7b043bb870fb22bf
Juan Castro <juamcastroh@gmail.com>
2026-05-12 02:38:27 -0300

    feat: comenzar la creacion del socket cliente, importando las libreris socket, threading y time

---
+++ client.py
@@ -0,0 +1,6 @@
+#=====================================================
+# BLOQUE 1: Librerias
+#=====================================================
+import socket    # Libreria que nos proporciona una interfaz de bajo ivel para la creacion de procesos de conexion
+import threading # permite ejecutar varias cosas al mismo tiempo en este caso leer y escribir sin que una bloquee a la otra
+import time      # Libreria que importa herramientos de tiempo para interactuar con el programa
